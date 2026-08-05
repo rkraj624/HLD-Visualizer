7397ae62994d2d8cd6a0c67ce87bc8805b068132
Ravi Kishan Raj <ravikishan624@gmail.com>
2026-08-05 11:41:36 +0530

    fixed deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // @ts-ignore vitest config
   test: {
     globals: true,
     environment: 'node',
